cbed234c8c0087a9ec0a85518c3e2b55fec81164
Cooper-Sainiak <cos97@pitt.edu>
2026-02-02 20:30:08 -0500

    Q2 Parts A-B

--- ps2.py
+++ ps2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from computeCost import computeCost
 from gradientDescent import gradientDescent
 from normalEqn import normalEqn
+import pandas as pd
 
 # D = 32
 
@@ -38,3 +39,51 @@ plt.show()
 # Q1 Part D Task 4
 thetaNorm = normalEqn(X,y)
 print("Theta:", thetaNorm)
+
+# Q2 Part B
+# When testing my code, you might have to put ford.csv into the same folder to make the path work
+df = pd.read_csv('ford.csv')
+feat = ['mileage','mpg']
+output = 'price'
+
+# mileage vs. price
+plt.figure(figsize=(8, 6))
+plt.scatter(df['mileage'], df['price'], alpha=0.5, color='blue')
+plt.xlabel('Mileage - mi')
+plt.ylabel('Price - $)')
+plt.title('Mileage vs Price')
+plt.grid(True, alpha=0.3)
+plt.show()
+
+# mpg vs. price
+plt.figure(figsize=(8, 6))
+plt.scatter(df['mpg'], df['price'], alpha=0.5, color='red')
+plt.xlabel('Fuel Usage - mpg')
+plt.ylabel('Price - $)')
+plt.title('MPG vs Price')
+plt.grid(True, alpha=0.3)
+plt.show()
+
+# Q2 Part C
+# to satisfy 5
+np.random.seed(32)
+
+# 2
+X_init = df[feat].values
+y = df[output].values
+feat_mean = np.mean(X_init,axis=0)
+feat_std = np.std(X_init,axis=0)
+X_standard = (X_init - feat_mean) / feat_std
+# 3
+m = len(y)
+X = np.column_stack([np.ones(m),X_standard])
+# 4
+index = np.random.permutation(m)
+split = int(0.9 * m)
+train = index[:split]
+test = index[split:]
+
+X_train = X[train]
+X_test = X[test]
+X_train = X[train]
+X_test = X[test]
